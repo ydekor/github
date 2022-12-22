9349a352b9cea27e31c50252efce413e2bfa42f4
ydekor <114324139+ydekor@users.noreply.github.com>
2022-12-22 15:35:24 +0300

    edit

---
+++ flutter/lw0/lw0/lib/fizzbuzz.dart
@@ -0,0 +1,22 @@
+int fizzbuzz(int number) {
+  if (number < 1) {
+    return number;
+  }
+  if (number % 3 == 0) {
+    print("fizz");
+  } else {
+    if (number % 5 == 0) {
+      print("buzz");
+    } else {
+      print(number);
+    }
+  }
+  if (number % 3 == 0 && number % 5 == 0) {
+    print("fizzbuzz");
+  }
+  return fizzbuzz(number - 1);
+}
+
+void main() {
+  fizzbuzz(15);
+}
--- php/lw2/src/lw2.php
+++ php/lw2/src/lw2.php
@@ -1,6 +1,8 @@
 <?php
-abstract class PizzaStore {
-    public function orderPizza(string $type) {
+abstract class PizzaStore
+{
+    public function orderPizza(string $type)
+    {
         $piz = $this->createPizza($type);
         $piz->prepare();
         $piz->cut();
@@ -8,27 +10,31 @@ abstract class PizzaStore {
     public abstract function createPizza(string $type): Pizza;
 }
 
-abstract class Pizza {
+abstract class Pizza
+{
     public string $name;
     public string $sauce;
     public array $toppings;
 
-    public function __construct(string $name, string $sus, array $top) {
+    public function __construct(string $name, string $sus, array $top)
+    {
         $this->name = $name;
         $this->sauce = $sus;
         $this->toppings = $top;
     }
 
-    public function cut(): string {
+    public function cut(): string
+    {
         $cutType = "по диагонали";
         return $cutType;
     }
 
-    public function prepare(): void {
+    public function prepare(): void
+    {
         echo "началась готовка пиццы {$this->name}";
         echo "Добавлен соус {$this->sauce}";
         echo "Добавлены топики: ";
-        for($i = 0; $i < count($this->toppings); $i++) {
+        for ($i = 0; $i < count($this->toppings); $i++) {
             echo "{$this->toppings[$i]}";
         }
     }
