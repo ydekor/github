dc86aba925b0e4336a29632565699c71000d9735
ydekor <114324139+ydekor@users.noreply.github.com>
2022-12-22 21:40:20 +0300

    first

--- flutter/lw0/lw0/lib/main.dart
+++ flutter/lw0/lw0/lib/main.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
 
-anagram<T>(String word, String word2) {
-  word = word.toUpperCase();
-  word2 = word2.toUpperCase();
-  if (word.substring(0, 1) != word2.substring(word.length - 1, 1)) {
-    return false;
+bool anagram({String first = "", String second = ""}) {
+  first = first.toLowerCase();
+  second = second.toLowerCase();
+
+  int i = first.length - 1;
+  int max1 = 0;
+
+  int j = 0;
+  int max2 = second.length - 1;
+
+  while (i >= max1 && j <= max2) {
+    if (!isAlphabetChar(first[i])) {
+      i--;
+      continue;
+    }
+    if (!isAlphabetChar(second[j])) {
+      j++;
+      continue;
+    }
+    if (first[i] != second[j]) {
+      return false;
+    }
+
+    i--;
+    j++;
   }
-  if (word.length < 2) {
-    return true;
-  }
-  word = word.substring(1, 1);
-  word2 = word.substring(word2.length - 1, 1);
-  return anagram(word, word2);
+  return true;
+}
+
+bool isAlphabetChar(String c) {
+  return c.contains(new RegExp('^[a-zA-z]')) ||
+      c.contains(new RegExp('^[а-яА-Я]'));
 }
 
 void main() {
-  anagram("finder", "Friend");
-  anagram("hello", "bye");
-  anagram("f? inder", "Friend");
+  print(anagram(first: "dneirf??", second: "Frien.d"));
+  print(anagram(first: "bye", second: "hello"));
+  print(anagram(first: "friend", second: "DNEIRF"));
 }
-
